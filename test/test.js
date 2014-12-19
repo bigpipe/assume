60f5b631427e752ca2aa70c06a228f46b574fc14
Arnout Kazemier <info@3rd-Eden.com>
2014-12-19 09:18:46 +0100

    [test] Move tests to one file for easy coverage reporting. [deps] Updated out of date dependencies

--- test/test.js
+++ test/test.js
@@ -82,3 +82,35 @@ describe('Assertions', function assertions() {
     });
   });
 });
+
+describe('i', function () {
+  'use strict';
+
+  var i = require('../');
+
+  describe('.hope', function () {
+    describe('.that', function () {
+      it('works', function () {
+        i.hope.that('foo').is.a('string');
+      });
+    });
+  });
+
+  describe('.expect', function () {
+    describe('.that', function () {
+      it('works', function () {
+        i.expect.that('foo').is.a('string');
+      });
+    });
+  });
+
+  describe('.sincerely', function () {
+    describe('.hope', function () {
+      describe('.that', function () {
+        it('works', function () {
+          i.sincerely.hope.that('foo').is.a('string');
+        });
+      });
+    });
+  });
+});
